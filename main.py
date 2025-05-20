83690adfd17d022e73516a71207bdc06c387f904
ditto616 <ditto616@gmail.com>
2025-05-20 14:25:16 -0400

    changes at lunch. Can't get Pygame to work for some reason. Test at home

--- main.py
+++ main.py
@@ -7,18 +7,26 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+   
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        ship.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        ship.draw(screen)
+        drawable.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
