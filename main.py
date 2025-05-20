2ac95120add25353ba2c987a3d6bd2bed995de90
ditto616 <ditto616@gmail.com>
2025-05-19 23:20:49 -0400

    Added turn

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        ship.update(dt)
         screen.fill("black")
         ship.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
