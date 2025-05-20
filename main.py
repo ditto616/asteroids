2438185d561277ebf365f76f3b20568c302113b8
ditto616 <ditto616@gmail.com>
2025-05-19 23:09:42 -0400

    Add player, CircleShape, PLAYER_RADIUS constant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame, constants
+import pygame, constants, player
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -7,13 +8,19 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.display.flip()
         screen.fill("black")
+        ship.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
